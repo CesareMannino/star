5c4e33b9753eee442fabce33b5e0b597a54d213a
cesare <cesaremannino@hotmail.it>
2020-10-13 21:27:30 +0200

    vs

--- src/starships/settings.py
+++ src/starships/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('/home/devmanny81/star/src/starships/secret_key.txt') as f:
+# on vs code  C:/Users/CESARE/astronavi/src/starships/secret_key.txt
+with open("C:/Users/CESARE/astronavi/src/starships/secret_key.txt") as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
