2fb9cc4b3e0e4b268a833af670c47b97f0fef260
cesare <cesaremannino@hotmail.it>
2020-12-07 21:33:59 +0100

    center

--- src/thrust/static/js/script.js
+++ src/thrust/static/js/script.js
@@ -1,55 +1,3 @@
-// MIO MANUAL COUNT DOWN
-
-//  Set the date we're counting down to
-//  all times need to be insert in UTC
-
-// electron
-// var countDownDate = new Date("dec 12, 2020 09:00:00").getTime();
-// falcon
-// var countDownDate1 = new Date("dec 5, 2020 16:39:00").getTime();
-// newshepard
-// var countDownDate2 = new Date("nov 13, 2020 08:00:00").getTime();
-// spaceshiptwo
-// var countDownDate3 = new Date("dec 1, 2020 12:00:00").getTime();
-
-
-
-// var arrElements = [
-// 		{"name": "electron", "eventdate": ""},
-// 		{"name": "falcon", "eventdate": ""},
-// 		{"name": "newshepard","eventdate": ""},
-// 		{"name": "spaceshiptwo","eventdate": ""},
-// 		{"name": "longmarch","eventdate": ""}
-// 	  ];
-
-// function setEventDate(eventname,eventdate){	
-// 	for (var i=0; i < arrElements.length; i++){ 
-// 		if(arrElements[i].name == eventname){
-// 			arrElements[i].eventdate = eventdate;			
-// 		}
-// 	}
-// }
-	  
-// Update the count down every 1 second
-// var x = setInterval(function() {
-  
-// 	var now = new Date().getTime();
-// 	var countDownDate = new Date("dec 12, 2020 09:00:00").getTime();
-// 	var distance = countDownDate - (now - 7200000);
-// 	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-// 	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-// 	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-// 	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-// 	document.getElementById("electron").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-// 	if (distance < 0) {		
-// 		document.getElementById("electron").innerHTML = "EXPIRED";
-// 	}
-    
-// }, 1000);
-
-
-
 var objToday = new Date(),
 	weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
 	dayOfWeek = weekday[objToday.getDay()],
@@ -64,3 +12,127 @@ var objToday = new Date(),
 var today = dayOfWeek + " " + dayOfMonth + " of " + curMonth + ", " + curYear;
 
 document.getElementsByTagName('h1')[0].innerHTML = today
+
+
+
+// Set the date we're counting down to
+// all times need to be insert in UTC
+
+// // electron
+// var countDownDate = new Date("Dec 12, 2020 09:00:00").getTime();
+// // falcon
+// var countDownDate1 = new Date("Dec 06, 2020 16:17:00").getTime();
+// // newshepard
+// var countDownDate2 = new Date("Nov 13, 2020 08:00:00").getTime();
+// // spaceshiptwo
+// var countDownDate3 = new Date("Dec 1, 2020 12:00:00").getTime();
+
+
+
+
+
+  // Update the count down every 1 second
+// var x = setInterval(function() {
+
+  // Get today's date and time in milliseconds new date give the today date and gettime() gives the milliseconds from 1970
+//   var now = new Date().getTime();
+  
+  // Find the distance between now and the count down date
+  // the 7200000 are milliseconds (2hrs) that need to be added to get as output the utc
+//   var distance = countDownDate - (now - 7200000);
+//   var distance1 = countDownDate1 - (now - 7200000);
+//   var distance2 = countDownDate2 - (now - 7200000);
+//   var distance3 = countDownDate3 - (now - 7200000);
+
+  // Time calculations for days, hours, minutes and seconds
+
+  // electron
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // falcon
+//   var days1 = Math.floor(distance1 / (1000 * 60 * 60 * 24));
+//   var hours1 = Math.floor((distance1 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes1 = Math.floor((distance1 % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds1 = Math.floor((distance1 % (1000 * 60)) / 1000);
+
+  // newshepard
+
+//   var days2 = Math.floor(distance2 / (1000 * 60 * 60 * 24));
+//   var hours2 = Math.floor((distance2 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes2 = Math.floor((distance2 % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds2 = Math.floor((distance2 % (1000 * 60)) / 1000);
+
+  // spaceshiptwo
+
+// var days3 = Math.floor(distance3 / (1000 * 60 * 60 * 24));
+//   var hours3 = Math.floor((distance3 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes3 = Math.floor((distance3 % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds3 = Math.floor((distance3 % (1000 * 60)) / 1000);
+
+
+
+  // Output the result in an element with id= rocket name
+//   document.getElementById("electron").innerHTML = days + "d " + hours + "h "
+//   + minutes + "m " + seconds + "s ";
+
+//   document.getElementById("falcon").innerHTML = days1 + "d " + hours1 + "h "
+//   + minutes1 + "m " + seconds1 + "s ";
+
+//   document.getElementById("newshepard").innerHTML = days2 + "d " + hours2 + "h "
+//   + minutes2 + "m " + seconds2 + "s ";
+
+//   document.getElementById("spaceshiptwo").innerHTML = days3 + "d " + hours3 + "h "
+//   + minutes3 + "m " + seconds3 + "s ";
+
+
+
+
+
+  // If the count down is over, write some text
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("electron").innerHTML = "EXPIRED";}
+
+//     if (distance1 < 0) {
+//       clearInterval(x);
+//     document.getElementById("falcon").innerHTML = "EXPIRED";}
+
+//     if (distance2 < 0) {
+//       clearInterval(x);
+//     document.getElementById("newshepard").innerHTML = "UNSCHEDULED";}
+
+//     if (distance3 < 0) {
+//       clearInterval(x);
+//     document.getElementById("spaceshiptwo").innerHTML = "UNSCHEDULED";}
+
+
+  //   document.getElementById("spaceshiptwo").innerHTML = "EXPIRED";
+
+  // }
+// }, 1000);
+
+// navbar burger menu click
+
+
+
+
+// top stick navbar
+// window.onscroll = function() {myfunction();};
+
+// var topnav = document.getElementById("topnav");
+// var sticky = topnav.offsetTop;
+
+// function myfunction() {
+//   if (window.pageYOffset >= sticky) {
+//     topnav.classList.add("sticky");
+//   } else {
+//     topnav.classList.remove("sticky");
+//   }
+// }
+
+
+
+
